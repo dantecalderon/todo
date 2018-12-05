54a043fd0da0850da05d412a820612fbdd2f78cb
dantehemerson <dantehemerson@gmail.com>
2018-12-05 18:36:56 -0500

    Add styles2

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,6 +23,21 @@ router.get('/delete/:id', async (req, res) => {
   res.redirect('/')
 })
 
+router.get('/edit/:id', async (req, res) => {
+  const { id } = req.params
+  const task = await Task.findById(id)
+  res.render('edit', {
+    task
+  })
+})
+
+router.post('/edit/:id', async (req, res) => {
+  const { id } = req.params
+  await Task.update({ _id: id}, req.body)
+  res.redirect('/')
+})
+
+
 router.get('/turn/:id', async (req, res) => {
   const { id } = req.params
   const task = await Task.findById(id)
