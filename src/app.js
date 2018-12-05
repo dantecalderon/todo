ff42d6cc4dad5b3030ddf23041a85cfcacb829dd
dantehemerson <dantehemerson@gmail.com>
2018-12-04 19:23:42 -0500

    Connect to DB

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+const indexRoutes = require('./routes/index')
+const app = express()
+
+
+mongoose.connect('mongodb://localhost/crud-mongo')
+	.then(db => console.log('DB Connected'))
+	.catch(err => console.log(err))
+
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+// Middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }))
+
+// Routes
+app.use('/', indexRoutes)
+
+app.listen(app.get('port'), () => {
+	console.log(`Server on port ${app.get('port')}`)
+})
