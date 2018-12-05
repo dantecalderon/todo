d8070d8d75c9b005f5763e16575532a2f8730045
dantehemerson <dantehemerson@gmail.com>
2018-12-05 09:14:04 -0500

    Add form

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,11 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
 	res.render('index')
-}) 
+})
+
+router.post('/add', (req, res) => {
+  console.log(req.body)
+  res.send('add')
+})
 
 module.exports = router
